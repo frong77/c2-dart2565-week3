a418c412c38170b80e1112efd37ed216a7a8fb4d
frong77 <nafitfit@gmail.com>
2022-05-30 12:31:27 +0700

    save

---
+++ bin/myapp301.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Locall Variable
+ var firstName ="Mark Zakerberg";//Dynamically Type checking
+ var age = 25;//Dnamically type checking 
+ const ceo = "Facebook"; //Statically Tpye Cheking
+ final price = 500;
+ print (firstName);
+ print(age);
+ print(price);
+}
